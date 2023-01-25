70acc9e98984362a7d64e5a97793f6dd8cc08a0b
unknown <fredericreact@gmail.com>
2023-01-25 18:22:36 +0800

    Working with Array & Object Types

--- basics.ts
+++ basics.ts
@@ -9,3 +9,22 @@ username = 'Max'
 let isInstructor: boolean;
 
 isInstructor = true;
+
+let hobbies: string[];
+
+hobbies = ['Sports', 'Cooking'];
+
+let person: {
+    name: string;
+    age: number;
+};
+
+person = {
+    name: 'Max',
+    age: 32
+}
+
+let people: {
+    name: string;
+    age: number;
+}[];
