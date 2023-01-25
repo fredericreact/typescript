07af31470afdd7ca9a65f5b29411664d5881ab2a
unknown <fredericreact@gmail.com>
2023-01-25 18:34:04 +0800

    Understanding Type Aliases

--- basics.ts
+++ basics.ts
@@ -1,29 +1,20 @@
-
-
-
 let age: number;
-
 age = 12;
 
 let username: string | string[];
-
 username = 'Max'
 
 let isInstructor: boolean;
-
 isInstructor = true;
 
 
-
 let hobbies: string[];
-
 hobbies = ['Sports', 'Cooking'];
 
 let person: {
     name: string;
     age: number;
 };
-
 person = {
     name: 'Max',
     age: 32
@@ -35,10 +26,7 @@ let people: {
 }[];
 
 
-
 let course = 'React course';
 
-
 let cours: string | number ='Angular';
-
 cours = 1234
