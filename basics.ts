6670faf5bf966a7379db70e693c8ef89b6fedf88
unknown <fredericreact@gmail.com>
2023-01-26 11:00:45 +0800

    Diving Into Generics

--- basics.ts
+++ basics.ts
@@ -36,3 +36,12 @@ function add(a: number, b: number) {
     return a + b;
 }
 
+function insertAtBeginning<T>(array: T[], value:T) {
+    const newArray = [value, ... array];
+    return newArray;
+}
+
+const demoArray = [1,2,3];
+
+const updatedArray = insertAtBeginning(demoArray,-1);
+const stringArray = insertAtBeginning(['a','b','c'], 'd')
